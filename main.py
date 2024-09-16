28a5fc509962695c55fdf4cc7733e7bc73c33997
HECTOR <hevonox@gmail.com>
2024-09-15 19:29:50 -0600

    creamos la ventana del juego

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+
+#Creamos la ventana del juego
+screen = turtle.Screen()
+#asignamos atributos a la ventana
+screen.title('Snake - Game ')
+screen.bgcolor('black')
+screen.setup(width=600, height=600)
+screen.tracer(0)

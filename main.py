f9baa1fc6a819796bf898053a3bba169cf4062a6
HECTOR <hevonox@gmail.com>
2024-09-15 19:49:23 -0600

    creamos la cabeza de la serpiente y la funcion para moverse

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import turtle
+import time
+
+posponer = 0.1
 
 #Creamos la ventana del juego
 screen = turtle.Screen()
@@ -7,3 +10,36 @@ screen.title('Snake - Game ')
 screen.bgcolor('black')
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+#Crear la cabeza de la serpiente
+head = turtle.Turtle()
+head.speed(0)
+head.shape('square')
+head.color('white')
+head.penup()
+head.goto(0,0)
+head.direction='stop'
+
+#funciones  
+
+def move ():
+    if head.direction == 'up':
+        y = head.ycor()
+        head.sety(y+20)
+    
+    elif head.direction == 'down':
+        y= head.ycor()
+        head.sety(y-20)
+
+    elif head.direction == 'left':
+        x= head.xcor()
+        head.setx(x-20)
+
+    elif head.direction == 'right':
+        x= head.xcor()
+        head.setx(x+20)
+
+while True:
+    screen.update()
+    move()
+    time.sleep(posponer)
